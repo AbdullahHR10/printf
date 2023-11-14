3f04eddc2ccb98dc184363d5f8c52b06879cbb19
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-14 09:33:33 -0800

    TEST

---
+++ printf.c
@@ -0,0 +1,53 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
+
+int _putchar(char c) {
+    return write(1, &c, 1);
+}
+
+int _printf(const char *format, ...) {
+    int count = 0;
+    va_list args;
+
+    va_start(args, format);
+
+    while (*format != '\0') {
+        if (*format == '%') {
+            format++;
+            if (*format == '\0') {
+                // Handle case where '%' is at the end of the format string
+                break;
+            }
+
+            if (*format == 'c') {
+                char ch = va_arg(args, int);
+                count += _putchar(ch);
+            } else if (*format == 's') {
+                char *str = va_arg(args, char*);
+                if (str == NULL) {
+                    str = "(null)";
+                }
+                while (*str != '\0') {
+                    count += _putchar(*str);
+                    str++;
+                }
+            } else if (*format == '%') {
+                count += _putchar('%');
+            } else {
+                // Unsupported specifier, treat it as a regular character
+                count += _putchar('%');
+                count += _putchar(*format);
+            }
+        } else {
+            count += _putchar(*format);
+        }
+
+        format++;
+    }
+
+    va_end(args);
+
+    return count;
+}
+
