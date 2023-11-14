56879e6a145d1ecd96e90b4cc9bf9f32a004c4d6
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-14 07:54:05 -0800

    Coded the function

---
+++ _printf.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _printf - custom printf function
+ * @format: format
+ */
+
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	va_list args;
+	char *str;
+
+	va_start(args, format);
+	str = va_arg(args, char*);
+	while (*format != '\0')
+	{
+
+		if (*format == '%')
+		{
+			format++;
+			count++;
+			if (*format == 'c')
+			{
+				_putchar(va_arg(args,int));
+				count++;
+			}
+			else if (*format == 's')
+			{
+				while (*str != '\0')
+				{
+					_putchar(*str);
+					str++;
+					count++;
+				}
+			}
+
+		}
+		else
+		{
+			_putchar(*format);
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
