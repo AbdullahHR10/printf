a0c1981edcc567a9e749ab1f4240014f3df76db7
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-14 08:19:39 -0800

    Test the results

--- _printf.c
+++ _printf.c
@@ -1,47 +1,50 @@
-#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * _printf - Custom printf function
- * @format: Format string
+ * _printf - custom printf function
+ * @format: format
  * Return: Count
  */
+
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    va_list args;
-    char *str;
+	int count = 0;
+	va_list args;
+	char *str;
 
-    va_start(args, format);
+	va_start(args, format);
+	str = va_arg(args, char*);
+	while (*format != '\0')
+	{
 
-    while (*format != '\0')
-    {
-        if (*format == '%')
-        {
-            format++;
-            count++;
-            switch (*format)
-            {
-                case 'c':
-                    _putchar(va_arg(args, int));
-                    count++;
-                    break;
-                case 's':
-                    str = va_arg(args, char*);
-                    count += print_string(str);
-                    break;
-                default:
-                    _putchar('%');
-                    break;
-            }
-        }
-        else
-        {
-            _putchar(*format);
-        }
-        format++;
-    }
+		if (*format == '%')
+		{
+			format++;
+			count++;
+			if (*format == 'c')
+			{
+				_putchar(va_arg(args, int));
+				count++;
+			}
+			else if (*format == 's')
+			{
+				while (*str != '\0')
+				{
+					print_string(str);
+					str++;
+					count++;
+				}
+			}
 
-    va_end(args);
-    return count;
+		}
+		else
+		{
+			_putchar(*format);
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
 }
