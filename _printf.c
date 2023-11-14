05b3de0d2d1864450dcd8aceb287170460d29faf
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2023-11-14 08:34:16 -0800

    Fixed some bugs

--- _printf.c
+++ _printf.c
@@ -1,50 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
-
-/**
- * _printf - custom printf function
- * @format: format
- * Return: Count
- */
-
 int _printf(const char *format, ...)
 {
-	int count = 0;
-	va_list args;
-	char *str;
+    int count = 0;
+    va_list args;
+    char *str;
 
-	va_start(args, format);
-	str = va_arg(args, char*);
-	while (*format != '\0')
-	{
+    va_start(args, format);
 
-		if (*format == '%')
-		{
-			format++;
-			count++;
-			if (*format == 'c')
-			{
-				_putchar(va_arg(args, int));
-				count++;
-			}
-			else if (*format == 's')
-			{
-				while (*str != '\0')
-				{
-					print_string(str);
-					str++;
-					count++;
-				}
-			}
+    while (*format != '\0')
+    {
+        if (*format == '%')
+        {
+            format++;
+            count++;
+            if (*format == 'c')
+            {
+                _putchar(va_arg(args, int));
+                count++;
+            }
+            else if (*format == 's')
+            {
+                str = va_arg(args, char*);
+                count += print_string(str);
+            }
+            else
+            {
+                _putchar(*format);
+            }
+        }
+        else
+        {
+            _putchar(*format);
+        }
+        format++;
+    }
 
-		}
-		else
-		{
-			_putchar(*format);
-		}
-		format++;
-	}
-	va_end(args);
-	return (count);
+    va_end(args);
+    return count;
 }
+
