bf2080c8e71e58cc007419ee1f619bb3fce4fe80
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-05-23 23:12:19 -0700

    Add print_int function

---
+++ print_int.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_int - Prints an integer for a custom printf function
+ * @i: Integer to be printed
+ *
+ * Return: Nothing
+ */
+
+void print_int(int i)
+{
+	int count = 0, digits[10], j;
+
+	if (i < 0)
+		_putchar('-');
+	if (i == 0)
+		_putchar('0');
+	while(i != 0)
+	{
+		digits[count] = i % 10;
+		i /= 10;
+		count++;
+	}
+	for (j = count - 1; j >= 0; j--)
+	{
+		_putchar(digits[j] + '0');
+	}
+}
