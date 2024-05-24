b45cf29790716bf161d5cd22d3870a46fa4cd355
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-05-23 23:29:50 -0700

    Add _printf function

---
+++ _printf.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _printf - Custom printf function
+ * @format: Character string  composed of 0 or more directives
+ *
+ * Return: The number of characters printed,
+ * Excluding the null byte used to end output to strings
+ */
+
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	va_list args;
+
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format) {
+				case 'c':
+					count += print_char(va_arg(args, int));
+					break;
+				case 's':
+					count += print_string(va_arg(args, char *));
+					break;
+				case 'd':
+				case 'i':
+					count += print_int (va_arg(args, int));
+					break;
+				default:
+					_putchar('%');
+					_putchar(*format);
+					count += 2;
+			}
+		} else {
+			_putchar(*format);
+			count++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
