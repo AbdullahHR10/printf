014da4a75ff480b394ebdf406a9bd9ebc47a550c
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-05-23 22:24:32 -0700

    Add print_char function

---
+++ print_char.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_char - Prints a character for a custom printf function
+ * @c: character to be printed
+ *
+ * Return: Nothing
+ */
+
+char print_char(char c)
+{
+	putchar(c);
+}
