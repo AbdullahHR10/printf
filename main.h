72430f697e85a78cd33b313b18085dca94a97be4
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-05-23 22:21:52 -0700

    Change file name and add standard lib

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
+char print_char(char c);
+int print_int(int i);
+char* print_string(char *s);
+float print_inti(float i);
+
+#endif
