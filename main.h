f1b36588804ac3629c7660d42baa25bd1c92a59d
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2025-02-07 18:27:45 +0200

    Add main header file.
    - Add format specifier struct.
    - Add functions prototypes.

---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * struct format_specifier - Structure that holds a format specifier.
+ * and its corresponding function to print the formatted output.
+ *
+ * @specifier: A character representing the format specifier.
+ * @print_function: A pointer to the function that handles printing the data.
+ */
+
+typedef struct format_specifier {
+    char specifier;
+    int (*print_function)(va_list *args);
+} format_specifier_t;
+
+int _printf(const char *format, ...);
+
+int _putchar(char c);
+int print_char(va_list *args);
+int print_string(va_list *args);
+
+#endif
