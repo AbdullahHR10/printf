ea4c91a7aa3932195b951ece21f2026d83a71587
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2025-02-07 18:35:09 +0200

    Add _printf function that mimics _printf.
    - Add support for %c and %s format specifiers.

---
+++ _printf.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+format_specifier_t specifiers[] = {
+    {'c', print_char},
+    {'s', print_string},
+    {0, NULL}
+};
+
+/**
+ * _printf - Function that mimics printf.
+ *
+ * @format: Format specifier.
+ * Return: 1 on success.
+ */
+
+int _printf(const char *format, ...) {
+    va_list args;
+    int i, j, char_count = 0;
+    char specifier;
+
+    if (format == NULL)
+        return (-1);
+
+    va_start(args, format);
+    for (i = 0; format[i] != '\0'; i++) {
+        if (format[i] == '%') {
+            i++;
+
+            if (format[i] == '\0')
+                return (-1);
+
+            if (format[i] == '%') {
+                _putchar(format[i]);
+                char_count++;
+                continue;
+            }
+            specifier = format[i];
+            j = 0;
+            /* Loop through the specifiers array */
+            while (specifiers[j].specifier != 0) {
+                if (specifiers[j].specifier == specifier) {
+                    char_count += specifiers[j].print_function(&args);
+                    break;
+                }
+                j++;
+            }
+            if (specifiers[j].specifier == 0) {
+                _putchar('%');
+                _putchar(specifier);
+                char_count += 2;
+            }
+        } else {
+            _putchar(format[i]);
+            char_count++;
+        }
+    }
+    va_end(args);
+    return (char_count);
+}
