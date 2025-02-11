1bab0e0503948a93d15cdf3fb6f66ab229aaf2bc
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2025-02-11 10:32:11 +0200

    Add a way to handle INT_MIN

--- print_int.c
+++ print_int.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * print_int - Prints an integer in _printf.
@@ -9,13 +10,21 @@
 
 int print_int(va_list *args) {
     int int_count = 0, num, index = 0;
-    char buffer[12];
+    char buffer[12], *int_min_str = "-2147483648";
 
     num = va_arg(*args, int);
 
     if (num == 0) {
         _putchar('0');
         return (1);
+    }
+
+    if (num == INT_MIN) {
+        while (int_min_str[int_count]) {
+            _putchar(int_min_str[int_count]);
+            int_count++;
+        }
+        return (int_count);
     }
 
     if (num < 0) {
