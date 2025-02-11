5a5bea8dadd1ca8cace2f5c461f415d42ca8c1ff
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2025-02-11 11:45:48 +0200

    Add print_octal that prints an integer that is converted to octal in _printf

---
+++ print_octal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_octal - Prints an integer that is converted to octal in _printf.
+ *
+ * @args: list of args.
+ * Return: The length of the integer (binary).
+ */
+
+int print_octal(va_list *args) {
+    unsigned int num;
+    int int_count = 0, index = 0;
+    char buffer[32];
+
+    num = va_arg(*args, unsigned int);
+
+    if (num == 0)
+    {
+        _putchar('0');
+        return (1);
+    }
+
+    while (num != 0) {
+        buffer[index++] = (num % 8) + '0';
+        num /= 8;
+    }
+
+    while (index > 0) {
+        _putchar(buffer[--index]);
+        int_count++;
+    }
+
+    return (int_count);
+}
