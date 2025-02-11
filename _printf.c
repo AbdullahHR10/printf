56a12514b1de5f3fbaec722069f26459afa5112e
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2025-02-11 13:53:37 +0200

    Add support for x and X format specifier to _printf

--- _printf.c
+++ _printf.c
@@ -7,6 +7,8 @@ format_specifier_t specifiers[] = {
     {'b', print_binary},
     {'u', print_unsigned},
     {'o', print_octal},
+    {'x', print_hex_lower},
+    {'X', print_hex_upper},
     {0, NULL}
 };
 
